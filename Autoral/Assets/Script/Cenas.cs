d765421210a72fd7463a52ea56de66eb85ebfe08
Aluno <aluno@cejoseleitelopes.com.br>
2018-08-15 12:14:29 -0300

    15.08.2018
    Menu

---
+++ Autoral/Assets/Script/Cenas.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cenas : MonoBehaviour
+{
+    public void Jogar()
+    {
+        SceneManager.LoadScene("Jogo");
+    }
+
+    public void Sobre()
+    {
+        SceneManager.LoadScene("Sobre");
+    }
+
+
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Application.Quit();
+
+        }
+
+    }
+}
