b1f68afeb412ea0fa6ee628adde76a4d0cbb2750
leninhacurvello@gmail.com <Hc@18062000>
2018-08-20 14:38:52 -0300

    20.08

---
+++ Autoral/Assets/Script/Move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    float vel;
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0))
+        {
+            transform.Translate(0, 0, vel);
+        }
+	}
+}
